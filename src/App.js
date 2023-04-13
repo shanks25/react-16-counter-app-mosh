b6ba16a041b11a5cd3674b4a88edc0a8dfc15af7
Kunal Rajput <kunal.rajput2511@gmail.com>
2023-04-13 12:38:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("App- Constructor", this.props);
+  }
+
+  componentDidMount() {
+    console.log("App - mounted");
+  }
+
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    if (counters[index].value) {
+      counters[index].value--;
+      this.setState({ counters });
+    }
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - Rendere");
+
+    return (
+      <>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          />
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
